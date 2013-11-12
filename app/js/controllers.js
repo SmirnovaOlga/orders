8a63295586b29db457dadcc4cd4dd59cdbf5f788
SmirnovaOlga <OlgaVSmir@gmail.com>
2013-11-12 13:02:30 +0400

    New directive counter.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,31 +12,6 @@ angular.module('ordersApp.controllers', []).
             $scope.path = $routeParams.path;
 
         }
-
-        function spinner(){
-            var spinner = $( "#spinner" ).spinner();
-            $( "#disable" ).click(function() {
-                if ( spinner.spinner( "option", "disabled" ) ) {
-                    spinner.spinner( "enable" );
-                } else {
-                    spinner.spinner( "disable" );
-                }
-            });
-            $( "#destroy" ).click(function() {
-                if ( spinner.data( "ui-spinner" ) ) {
-                    spinner.spinner( "destroy" );
-                } else {
-                    spinner.spinner();
-                }
-            });
-            $( "#getvalue" ).click(function() {
-                alert( spinner.spinner( "value" ) );
-            });
-            $( "#setvalue" ).click(function() {
-                spinner.spinner( "value", 5 );
-            });
-        }
-
     }]);
 
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,38 @@
 
 /* Directives */
 
-
 angular.module('ordersApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+
+angular.module('ordersApp.directives', []).
+    directive('counter', function() {
+    return {
+        template:  '<p class="article">Count'+
+            '<input type="text" ng-model="num" class="counter" value={{num}}>'+
+            '<i class="icon-arrow-up arrow-up" ng-click="up()"></i><i class="icon-arrow-down arrow-down"  ng-click="down()"></i></p>',
+
+        replace: false,
+
+        link: function(scope, element, controller) {
+            scope.num = 0;
+            scope.up = function()
+            {
+                var number = parseInt(scope.num);
+                scope.num = ++number;
+            }
+
+            scope.down = function()
+            {
+                var number = parseInt(scope.num);
+                if(number > 0)
+                    scope.num = --number;
+                else
+                    scope.num = 0;
+            }
+        }
+        }
+    });
